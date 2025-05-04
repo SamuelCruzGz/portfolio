da4ceb3aca2e45f5da08d1620ee0ba5ef68f6702
SamuelCruzGz <samuel.sebas46@gmail.com>
2025-05-04 17:37:35 -0600

    Finishing navigation bar top

--- portfolio/src/components/NavbarTop.tsx
+++ portfolio/src/components/NavbarTop.tsx
@@ -1,13 +1,16 @@
 export default function Home() {
   return (
-    <div className="relative pb-6 animate-fade-in-down">
-    <div className=" flex justify-around px-5 pt-4 text-2xl">
-        <h1 className="font-orbitron text-5xl text-color-font text-glow">Samuel Sebastián Cruz González T</h1>
-        <p className="font-game text-3xl text-secondary-grey text-right ml-4 mt-2 text-glow">Desarrollador de Software</p>        
-    </div>    
-    <div className="w-full flex justify-center mt-4">
-        <div className="glow-bar animate-none"></div>
-    </div>    
-    </div>
+    <section aria-labelledby="intro-title">
+      <div className="relative pb-6 animate-fade-in-down">
+      <div className="flex flex-col sm:flex-row sm:justify-around items-center px-5 pt-4 text-2xl text-center sm:text-left">
+        
+          <h1 id="intro-title" className="font-orbitron text-color-font text-glow sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">Samuel Sebastián Cruz González</h1>
+          <p className="font-game text-secondary-grey text-right ml-4 mt-2 text-glow sm:text-sm md:text-xl lg:text-2xl xl:text-3xl">Desarrollador de Software</p>              
+      </div>    
+      <div className="w-full flex justify-center mt-4">
+          <div className="glow-bar animate-none"></div>
+      </div>    
+      </div>
+    </section>        
   );
 }
